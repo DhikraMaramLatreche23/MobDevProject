8c63bd9fd3f731169bb6c25a689ca612642531e3
insafdel <129179408+insafdel@users.noreply.github.com>
2024-12-06 08:11:26 -1100

    Update colors.dart

--- mydoctorpage/lib/views/themes/colors.dart
+++ mydoctorpage/lib/views/themes/colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 const purple = Color(0xFF9C27B0);
 const dark_purple = Color(0xFF4A148C);
-var gradientColor1 = Color.fromRGBO(156, 39, 176, 1).withOpacity(0.5);
+var gradientColor1 = const Color.fromARGB(255, 53, 115, 167).withOpacity(0.5);
 var gradientColor2 = const Color.fromARGB(255, 194, 214, 240).withOpacity(0.5);
 var backgroundGradient1 =
     const Color.fromARGB(255, 149, 115, 155).withOpacity(0.5);
@@ -12,5 +12,4 @@ var backgroundGradient2 =
 const dark_bleu = Color.fromARGB(255, 3, 4, 94);
 const light_bleu = Color.fromARGB(255,0 , 180 ,216);
 const info_bleu = Color.fromARGB(255, 0, 119, 182);
-var blue = Color.fromARGB(255, 53, 115, 167);
-
+var blue = const Color.fromARGB(255, 53, 115, 167);
