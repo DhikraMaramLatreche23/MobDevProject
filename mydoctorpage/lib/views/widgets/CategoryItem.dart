a894bc3aef0ba36f197ba7f3a6418a2d5bfbfeef
DhikraMaramLatreche23 <145930013+DhikraMaramLatreche23@users.noreply.github.com>
2024-12-06 17:48:01 +0100

    Update CategoryItem.dart

--- mydoctorpage/lib/views/widgets/CategoryItem.dart
+++ mydoctorpage/lib/views/widgets/CategoryItem.dart
@@ -6,38 +6,40 @@ class CategoryItem extends StatelessWidget {
   final VoidCallback onTap;
 
   const CategoryItem({
-    Key? key,
+    super.key,
     required this.title,
     required this.imagePath,
     required this.onTap,
   });
 
   @override
-
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            width: 80,
-            height: 80,
-            decoration: BoxDecoration(
-              color: Colors.blue[50],
-              borderRadius: BorderRadius.circular(20),
+    return GestureDetector(
+      onTap: onTap, // Trigger the onTap callback when tapped
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: 80,
+              height: 80,
+              decoration: BoxDecoration(
+                color: Colors.blue[50],
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(imagePath, fit: BoxFit.contain),
+              ),
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset(imagePath, fit: BoxFit.contain),
+            const SizedBox(height: 10),
+            Text(
+              title,
+              style: const TextStyle(fontSize: 10, fontWeight: FontWeight.w500),
             ),
-          ),
-          const SizedBox(height: 10),
-          Text(
-            title,
-            style: const TextStyle(fontSize: 10, fontWeight: FontWeight.w500),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
