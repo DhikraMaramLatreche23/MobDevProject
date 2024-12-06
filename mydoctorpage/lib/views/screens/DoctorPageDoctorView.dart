c5110d8427e04bf78ca699b516d8e4c1d6cb2d42
insafdel <129179408+insafdel@users.noreply.github.com>
2024-12-06 12:10:36 -1100

    Update DoctorPageDoctorView.dart

--- mydoctorpage/lib/views/screens/DoctorPageDoctorView.dart
+++ mydoctorpage/lib/views/screens/DoctorPageDoctorView.dart
@@ -28,7 +28,7 @@ class _DoctorDoctorState extends State<DoctorDoctor> {
 // );
 
     return Scaffold(
-        appBar: AppBar(title: const Text("")),
+        // appBar: AppBar(title: const Text("")),
         body: Container(
            decoration: BoxDecoration(
           
