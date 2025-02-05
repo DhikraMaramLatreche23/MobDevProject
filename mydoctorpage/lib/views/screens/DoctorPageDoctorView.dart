b9bd36a50a8f4c906482f34398847180e7dceac4
insafdel <129179408+insafdel@users.noreply.github.com>
2025-02-05 07:08:02 -1100

    Update DoctorPageDoctorView.dart

--- mydoctorpage/lib/views/screens/DoctorPageDoctorView.dart
+++ mydoctorpage/lib/views/screens/DoctorPageDoctorView.dart
@@ -1,46 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mydoctorpage/bloc/calendar_bloc.dart';
+import 'package:mydoctorpage/bloc/calendar_state.dart';
 import 'package:mydoctorpage/views/themes/colors.dart';
 import 'package:mydoctorpage/views/widgets/DoctorcardModify.dart';
-import '../widgets/CalendarDoctor.dart';
+import 'package:mydoctorpage/views/widgets/CalendarDoctor.dart';
 
-class DoctorDoctor extends StatefulWidget {
+class DoctorDoctor extends StatelessWidget {
   const DoctorDoctor({super.key});
 
   @override
-  State<DoctorDoctor> createState() => _DoctorDoctorState();
-}
-
-class _DoctorDoctorState extends State<DoctorDoctor> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        // decoration: BoxDecoration(
-        //   gradient: LinearGradient(
-        //     colors: [backgroundGradient1, backgroundGradient2],
-        //     begin: Alignment.bottomRight,
-        //     end: Alignment.topLeft,
-        //   ),
-        // ),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const DoctorCardModify(),
-              const SizedBox(height: 20),
-              Center(
-                child: Text(
-                  "Jours Disponibles",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: dark_bleu,
-                    fontSize: 20,
+      body: BlocProvider(
+        create: (context) => CalendarBloc(14),
+        child: Container(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const DoctorCardModify(),
+                const SizedBox(height: 20),
+                Center(
+                  child: Text(
+                    "Jours Disponibles",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: dark_bleu,
+                      fontSize: 20,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              CalendarDoctor(),
-              const SizedBox(height: 20),
-            ],
+                const SizedBox(height: 20),
+                const CalendarDoctor(),
+                const SizedBox(height: 20),
+              ],
+            ),
           ),
         ),
       ),
