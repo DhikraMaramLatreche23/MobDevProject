af0033e2f48448f09efa61cf66e09b2f52d637f0
DhikraMaramLatreche23 <145930013+DhikraMaramLatreche23@users.noreply.github.com>
2025-02-07 01:52:55 +0100

    Update  Routes in main.dart

--- mydoctorpage/lib/main.dart
+++ mydoctorpage/lib/main.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:mydoctorpage/views/screens/AllCategories.dart';
 import 'package:mydoctorpage/views/screens/DoctorPageDoctorView.dart';
 import 'package:mydoctorpage/views/screens/DoctorPagePatientView.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mydoctorpage/views/screens/HomePage.dart';
+import 'package:mydoctorpage/views/screens/CategoryPage.dart';
+
+import 'package:mydoctorpage/views/screens/CategoryHeart.dart';
+import 'package:mydoctorpage/views/screens/CategoryBrain.dart';
+import 'package:mydoctorpage/views/screens/CategoryEye.dart';
+import 'package:mydoctorpage/views/screens/CategoryKidneys.dart';
+import 'package:mydoctorpage/views/screens/CategoryGastro.dart';
+import 'package:mydoctorpage/views/screens/CategoryBones.dart';
+import 'package:mydoctorpage/views/screens/CategoryPneumo.dart';
+import 'views/screens/signchoose_screen/signchoose_screen.dart';
+import 'package:mydoctorpage/views/screens/doc_modify_profile.dart';
+import 'views/screens/signup/signUpDoctorPageOne.dart';
+import 'views/screens/signup/signUpPatientPageOne.dart';
+import 'views/screens/signup/signUpPatientPageTwo.dart';
+import 'views/screens/signup/signUpDoctorPageTwo.dart';
+import 'views/screens/login/loginDoctor.dart';
+import 'views/screens/login/loginPatient.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mydoctorpage/bloc/category_bloc.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    BlocProvider(
+      create: (context) => CategoryBloc(),
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -14,17 +39,30 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         textTheme: GoogleFonts.poppinsTextTheme(),
       ),
-      // home: Scaffold(
-      //   body: HomePage(),
-      // ),
-      initialRoute: '/home',
+      home: const SignChooseScreen(),
       routes: {
-        '/home': (context) => HomePage(),
-        '/DoctorPagePatientView': (context) => DoctorDoctor(),
-        '/rdv': (context) => DoctorDoctor(),
+        // HomePage.pageRoute: (ctx) => const HomePage(),
+        SignChooseScreen.pageRoute: (ctx) => const SignChooseScreen(),
+        SignUpPatient.pageRoute: (ctx) => const SignUpPatient(),
+        SignPatient.pageRoute: (ctx) => const SignPatient(),
+        SignUpDoctor.pageRoute: (ctx) => const SignUpDoctor(),
+        SignDoctor.pageRoute: (ctx) => const SignDoctor(),
+        Logindoctor.pageRoute: (ctx) => const Logindoctor(),
+        Loginpatient.pageRoute: (ctx) => const Loginpatient(),
+        '/home': (context) => const HomePage(),
+        HomePage.routeName: (context) => const HomePage(),
+        '/DoctorPagePatientView': (context) => const DoctorPage(),
+        '/rdv': (context) => const DoctorDoctor(),
+        '/allcategories': (context) => const Allcategories(),
+        '/doc_modify_profile': (context) => const DocModifyProfile(),
+        '/CategoryPage': (context) => BlocProvider.value(
+              value: BlocProvider.of<CategoryBloc>(context),
+              child: const CategoryPage(),
+            ),
       },
     );
   }
